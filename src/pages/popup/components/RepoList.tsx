406212418f007b6c5b48c809e6ccbf38135945a9
Piotr Zadka <piotr.zadka@sky.uk>
2025-04-24 14:09:34 +0100

    updated add button

--- src/pages/popup/components/RepoList.tsx
+++ src/pages/popup/components/RepoList.tsx
@@ -1,49 +1,58 @@
-import { useContext } from 'react';
-import { LandingPageContext } from '@src/context/landing-page-context';
+import { useContext } from "react";
+import { LandingPageContext } from "@src/context/landing-page-context";
 import {
-  List,
-  ListItemText,
-  ListItemButton,
-  Divider,
-  Button,
-  Box,
-  Typography,
-  Tooltip,
-} from '@mui/material';
-import type { RepoListProps } from '@src/types';
+	List,
+	ListItemText,
+	ListItemButton,
+	Divider,
+	Button,
+	Box,
+	Typography,
+	Tooltip,
+} from "@mui/material";
+import type { RepoListProps } from "@src/types";
 
 export const RepoList = ({ repos, onRemove }: RepoListProps) => {
-  const { setSelectedRepo } = useContext(LandingPageContext);
+	const { setSelectedRepo } = useContext(LandingPageContext);
 
-  return (
-    <div>
-      <List>
-        <ListItemText primary="Your repositories" />
-      </List>
-      <Divider />
-      <List>
-        {repos.map((repo) => (
-          <Box key={repo} display="flex" alignItems="center" gap={2}>
-            <ListItemButton onClick={() => setSelectedRepo(repo)} key={repo}>
-              <Tooltip title={repo} arrow>
-                <Typography
-                  sx={{
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    whiteSpace: 'nowrap',
-                    maxWidth: '270px',
-                  }}
-                >
-                  <ListItemText primary={repo} />
-                </Typography>
-              </Tooltip>
-            </ListItemButton>
-            <Button onClick={() => onRemove(repo)} variant="text">
-              Remove
-            </Button>
-          </Box>
-        ))}
-      </List>
-    </div>
-  );
+	return (
+		<div>
+			<List>
+				<ListItemText primary="Your repositories" />
+			</List>
+			<Divider />
+			{repos.length === 0 && (
+				<Typography
+					variant="body1"
+					sx={{ padding: 2 }}
+					style={{ whiteSpace: "nowrap" }}
+				>
+					You haven't added any repositories yet.
+				</Typography>
+			)}
+			<List>
+				{repos.map((repo) => (
+					<Box key={repo} display="flex" alignItems="center" gap={2}>
+						<ListItemButton onClick={() => setSelectedRepo(repo)} key={repo}>
+							<Tooltip title={repo} arrow>
+								<Typography
+									sx={{
+										overflow: "hidden",
+										textOverflow: "ellipsis",
+										whiteSpace: "nowrap",
+										maxWidth: "270px",
+									}}
+								>
+									<ListItemText primary={repo} />
+								</Typography>
+							</Tooltip>
+						</ListItemButton>
+						<Button onClick={() => onRemove(repo)} variant="text">
+							Remove
+						</Button>
+					</Box>
+				))}
+			</List>
+		</div>
+	);
 };
