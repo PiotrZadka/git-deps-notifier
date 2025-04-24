625ef1eb4468c685770f7cbbd60ddbde9bbe9075
Piotr Zadka <piotr.zadka@sky.uk>
2025-04-24 11:18:50 +0100

    removed auth implementation - not working

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,32 +1,18 @@
-import { useState, useEffect } from 'react';
-import '@pages/popup/index.css';
-import { Repositories } from './Views/Repositories';
-import { Login } from './Views/Login';
-import { LandingPageContext } from '../../context/landing-page-context';
+import { useState, useEffect } from "react";
+import "@pages/popup/index.css";
+import { Repositories } from "./Views/Repositories";
+import { Login } from "./Views/Login";
+import { LandingPageContext } from "../../context/landing-page-context";
 
 export const Popup = () => {
-  const [selectedRepo, setSelectedRepo] = useState('');
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [selectedRepo, setSelectedRepo] = useState("");
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    const checkLoginStatus = () => {
-      const token = localStorage.getItem('github_access_token');
-      setIsLoggedIn(!!token);
-    };
-
-    checkLoginStatus();
-
-    window.addEventListener('storage', checkLoginStatus);
-    return () => {
-      window.removeEventListener('storage', checkLoginStatus);
-    };
-  }, []);
-
-  return (
-    <>
-      <LandingPageContext.Provider value={{ selectedRepo, setSelectedRepo }}>
-        {!isLoggedIn ? <Login /> : <Repositories />}
-      </LandingPageContext.Provider>
-    </>
-  );
+	return (
+		<>
+			<LandingPageContext.Provider value={{ selectedRepo, setSelectedRepo }}>
+				{!isLoggedIn ? <Login /> : <Repositories />}
+			</LandingPageContext.Provider>
+		</>
+	);
 };
--- src/pages/popup/Views/Login.tsx
+++ src/pages/popup/Views/Login.tsx
@@ -1,85 +1,31 @@
-import { Button, Box, Typography, Divider } from '@mui/material';
-import { FaGithub } from 'react-icons/fa';
-import axios from 'axios';
+import { Button, Box, Typography, Divider } from "@mui/material";
+import { FaGithub } from "react-icons/fa";
+import axios from "axios";
 
 export const Login = () => {
-  const handleGitHubLogin = () => {
-    const clientId = import.meta.env.VITE_OAUTH_CLIENT_ID;
-    const redirectUri = chrome.identity.getRedirectURL('oauth2');
-    const scope = 'repo user';
+	const handleGitHubLogin = () => {};
 
-    const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
-
-    chrome.identity.launchWebAuthFlow(
-      {
-        url: authUrl,
-        interactive: true,
-      },
-      (redirectUrl) => {
-        if (redirectUrl) {
-          const urlParams = new URLSearchParams(new URL(redirectUrl).search);
-          const code = urlParams.get('code');
-          if (code) {
-            handleAuthCode(code);
-          }
-        } else {
-          console.error('Authorization failed or was canceled.');
-        }
-      }
-    );
-  };
-
-  const handleAuthCode = async (code) => {
-    try {
-      // GitHub token endpoint
-      const tokenUrl = 'https://github.com/login/oauth/access_token';
-
-      // Replace with your actual client ID and client secret
-      const clientId = import.meta.env.VITE_OAUTH_CLIENT_ID;
-      const clientSecret = import.meta.env.VITE_OAUTH_CLIENT_SECRET;
-
-      const response = await axios.post(
-        tokenUrl,
-        {
-          client_id: clientId,
-          client_secret: clientSecret,
-          code,
-        },
-        {
-          headers: {
-            Accept: 'application/json',
-          },
-        }
-      );
-      const accessToken = response.data.access_token;
-      localStorage.setItem('github_access_token', accessToken);
-      window.dispatchEvent(new Event('storage'));
-    } catch (error) {
-      console.error('Error handling auth code:', error);
-    }
-  };
-
-  return (
-    <Box sx={{ m: 2, textAlign: 'center' }}>
-      <Typography variant="h5" style={{ whiteSpace: 'nowrap' }}>
-        Github Dependency Notifier
-      </Typography>
-      <Divider />
-      <Typography variant="body1" sx={{ mt: 1, mb: 1, textAlign: 'left' }}>
-        To access your repositories and notify you about dependency updates,
-        please log in with your GitHub account.
-      </Typography>
-      <Divider />
-      <Box sx={{ mt: 1 }}>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleGitHubLogin}
-          startIcon={<FaGithub />}
-        >
-          Login with GitHub
-        </Button>
-      </Box>
-    </Box>
-  );
+	return (
+		<Box sx={{ m: 2, textAlign: "center" }}>
+			<Typography variant="h5" style={{ whiteSpace: "nowrap" }}>
+				Github Dependency Notifier
+			</Typography>
+			<Divider />
+			<Typography variant="body1" sx={{ mt: 1, mb: 1, textAlign: "left" }}>
+				To access your repositories and notify you about dependency updates,
+				please log in with your GitHub account.
+			</Typography>
+			<Divider />
+			<Box sx={{ mt: 1 }}>
+				<Button
+					variant="contained"
+					color="primary"
+					onClick={handleGitHubLogin}
+					startIcon={<FaGithub />}
+				>
+					Login with GitHub
+				</Button>
+			</Box>
+		</Box>
+	);
 };
